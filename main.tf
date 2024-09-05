41326fc1fc607b33cab4b49313122f22ed3a3608
harshrajurkar <harshrajurkar201@gmail.com>
2024-09-05 19:15:29 +0530

    added IGW

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# Provider configuration
 provider "aws" {
   region = var.aws_region
 }
@@ -32,6 +31,39 @@ resource "aws_subnet" "medusa_subnet_2" {
   tags = {
     Name = "medusa-subnet-2"
   }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "medusa_igw" {
+  vpc_id = aws_vpc.medusa_vpc.id
+
+  tags = {
+    Name = "medusa-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "medusa_route_table" {
+  vpc_id = aws_vpc.medusa_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.medusa_igw.id
+  }
+
+  tags = {
+    Name = "medusa-route-table"
+  }
+}
+
+resource "aws_route_table_association" "medusa_route_table_association_1" {
+  subnet_id      = aws_subnet.medusa_subnet_1.id
+  route_table_id = aws_route_table.medusa_route_table.id
+}
+
+resource "aws_route_table_association" "medusa_route_table_association_2" {
+  subnet_id      = aws_subnet.medusa_subnet_2.id
+  route_table_id = aws_route_table.medusa_route_table.id
 }
 
 # Security group
@@ -87,11 +119,7 @@ resource "aws_ecs_task_definition" "medusa_task" {
         {
           name  = "DATABASE_URL"
           value = var.database_url
-        },
-        # {
-        #   name  = "REDIS_URL"
-        #   value = var.redis_url
-        # }
+        }
       ]
       logConfiguration = {
         logDriver = "awslogs"
